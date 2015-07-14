82886a390ac77c6b1da69d451485ad0a32c38ee3
JingJie Li <wentixiaogege@163.com>
2015-07-13 20:51:01 -0700

    Monday working version

--- src/main/java/edu/itu/course/PropertyReading.java
+++ src/main/java/edu/itu/course/PropertyReading.java
@@ -34,6 +34,21 @@ public class PropertyReading {
 		}
 		return typeStr;
 	}
+	
+	public String getServerBroadcastAddress() {
+		Properties props = new Properties();
+		try {
+			props.load(PropertyReading.class.getResourceAsStream("/xbee.properties"));
+		} catch (IOException e) {
+			System.out.println("Could not find sensors properties!");
+			e.printStackTrace();
+		}
+		String typeStr = props.getProperty("ServerBroadcastAddress");
+		if (null == typeStr) {
+			return "";
+		}
+		return typeStr;
+	}
 	public String getXbeeDevice() {
 		Properties props = new Properties();
 		try {
